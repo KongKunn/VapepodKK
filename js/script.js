ebb3e0beabc5c4b76c36b92474f3898510f2c75b
KongKunn <90901186+KongKunn@users.noreply.github.com>
2022-10-21 23:35:02 +0700

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,29 +1,37 @@
-let menu = document.querySelector('#menu-bar')
-let navbar = document.querySelector('.navbar')
+let menu = document.querySelector("#menu-bar");
+let navbar = document.querySelector(".navbar");
 
-menu.onclick = () =>{
-    menu.classList.toggle('fa-times');
-    navbar.classList.toggle('active');
+menu.onclick = () => {
+  menu.classList.toggle("fa-times");
+  navbar.classList.toggle("active");
+};
+let items = document.querySelectorAll("li");
+items.forEach((li) => {
+  li.addEventListener("click", function (e) {
+    document.getElementById("action").style.left = e.target.offsetLeft + "px";
+    items.forEach((li_hide) => {
+      li_hide.classList.remove("active");
+    });
+    this.classList.add("active");
+  });
+});
 
+window.onscroll = () => {
+  menu.classList.remove("fa-times");
+  navbar.classList.remove("active");
+
+  if (window.scrollY > 60) {
+    document.querySelector("#scroll-top").classList.add("active");
+  } else {
+    document.querySelector("#scroll-top").classList.remove("active");
+  }
+};
+function loader() {
+  document.querySelector(".loader-container").classList.add("fade-out");
 }
 
-window.onscroll = () =>{
-
-    menu.classList.remove('fa-times');
-    navbar.classList.remove('active');
-  
-    if(window.scrollY > 60){
-      document.querySelector('#scroll-top').classList.add('active');
-    }else{
-      document.querySelector('#scroll-top').classList.remove('active');
-    }
+function fadeOut() {
+  setInterval(loader, 2000);
 }
-function loader(){
-    document.querySelector('.loader-container').classList.add('fade-out');
-  }
-  
-  function fadeOut(){
-    setInterval(loader, 2000);
-  }
-  
-  window.onload = fadeOut();
+
+window.onload = fadeOut();
